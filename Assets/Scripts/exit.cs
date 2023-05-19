e15ccefbc2fd9da15f79e596d9a54ed99ee2566d
José Adrián Porres Brugué <jose.porres@utec.edu.pe>
2023-05-19 17:02:54 -0500

    feat: integration done

--- Assets/Scripts/exit.cs
+++ Assets/Scripts/exit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class exit : MonoBehaviour
+public class Exit : MonoBehaviour
 {
     public void ExitApp()
     {
